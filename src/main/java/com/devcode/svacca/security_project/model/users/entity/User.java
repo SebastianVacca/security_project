004ba3c1132b57c1947a3c84b31b7f0e265ff9cf
Juan Vacca <clanbs11.12@gmail.com>
2026-04-12 12:00:32 -0500

    creación de la entidades User y Role con sus relaciones entre si ya definidas

---
+++ src/main/java/com/devcode/svacca/security_project/model/users/entity/User.java
@@ -0,0 +1,45 @@
+package com.devcode.svacca.security_project.model.users.entity;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+import org.springframework.context.support.BeanDefinitionDsl.Role;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="users")
+public class User {
+    
+    @Id
+    private String username;
+
+    @Column(length = 100, nullable = false, unique = true)
+    private String email;
+
+    @Column(length = 100, nullable = false)
+    private String password;
+
+    @Column(name = "full_name")
+    private String name;
+
+    @Column(name = "hire_date")
+    private LocalDate hireDate;
+
+    @ManyToMany
+    @JoinTable(name = "user_roles",
+     joinColumns = @JoinColumn(columnDefinition = "user_id"),
+     inverseJoinColumns = @JoinColumn(columnDefinition = "role_id")
+    )
+    private Set<Role> roles;
+
+    private Boolean active;
+}
