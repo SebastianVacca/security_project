c2cf5c05d30023531367b1cfa57607f2214efd61
Juan Vacca <clanbs11.12@gmail.com>
2026-04-12 21:35:48 -0500

    Configuración de atributos de ResponseUser

--- src/main/java/com/devcode/svacca/security_project/model/users/dto/ResponseUser.java
+++ src/main/java/com/devcode/svacca/security_project/model/users/dto/ResponseUser.java
@@ -1,5 +1,26 @@
 package com.devcode.svacca.security_project.model.users.dto;
 
-public record ResponseUser() {
+import java.time.LocalDate;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
+
+public record ResponseUser(
+
+    String username,
+
+    String email,
+
+    String name,
+
+    @JsonFormat(shape = Shape.STRING, pattern = "yyyy-MM-dd")
+    @JsonProperty(value = "hire_date")
+    LocalDate hireDate,
+
+    Boolean active,
+    
+    Set<String> roles) {
     
 }
